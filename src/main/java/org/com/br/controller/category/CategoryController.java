01c74c3ba8466ee175516b8eebc65ea9ad8adde2
Alexandre <alexandrec@alu.ufc.br>
2026-04-07 15:25:38 -0300

    feat: retornar todas as categorias

--- src/main/java/org/com/br/controller/category/CategoryController.java
+++ src/main/java/org/com/br/controller/category/CategoryController.java
@@ -1,10 +1,7 @@
 package org.com.br.controller.category;
 
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.com.br.entity.category.CategoryEntity;
@@ -25,5 +22,14 @@ public class CategoryController {
     @Transactional
     public Response createCategory(final CategoryEntity category){
         return Response.ok(this.service.create(category)).build();
+    }
+
+    @GET
+    public Response findAll(
+            @QueryParam("page") @DefaultValue("0") Integer page,
+            @QueryParam("perSize")@DefaultValue("10") Integer pageSize
+    ) {
+        final var categories = service.findAll(page, pageSize);
+        return Response.ok(categories).build();
     }
 }
--- src/main/java/org/com/br/service/category/CategoryService.java
+++ src/main/java/org/com/br/service/category/CategoryService.java
@@ -3,11 +3,19 @@ package org.com.br.service.category;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.com.br.entity.category.CategoryEntity;
 
+import java.util.List;
+
 @ApplicationScoped
 public class CategoryService {
 
     public CategoryEntity create(final CategoryEntity category){
         CategoryEntity.persist(category);
         return category;
+    }
+
+    public List<CategoryEntity> findAll(final Integer page, final Integer pageSize) {
+        return CategoryEntity.findAll()
+                .page(page, pageSize)
+                .list();
     }
 }
